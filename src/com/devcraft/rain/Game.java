ead17599175e8e6be1720d3a32c349733a4927f9
Daniel Villalobos <mdvillalobos@gmail.com>
2015-03-31 23:04:03 +0200

    Implemented render method on Screen class for rendering pixels

--- src/com/devcraft/rain/Game.java
+++ src/com/devcraft/rain/Game.java
@@ -90,10 +90,15 @@ public class Game extends Canvas implements Runnable{
 			return;
 		}
 		
+		screen.render();
+		
+		for (int i = 0; i < pixels.length; i++) {
+			pixels[i] = screen.pixels[i];
+		}
+		
 		Graphics g = bs.getDrawGraphics();
 		// All drawing code goes here... 
-		g.setColor(Color.BLACK);
-		g.fillRect(0, 0, getWidth(), getHeight());
+		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
 		// ... before call dispose()
 		
 		g.dispose();
